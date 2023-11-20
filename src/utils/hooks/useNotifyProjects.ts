04a1d7ae853cdec72319ecf0d28768f8e3e0d3b8
enesozturk <enesozturk.d@gmail.com>
2023-11-20 11:51:13 +0300

    refactor: update notify projects hook

--- src/utils/hooks/useNotifyProjects.ts
+++ src/utils/hooks/useNotifyProjects.ts
@@ -3,13 +3,16 @@ import SettingsContext from '../../contexts/SettingsContext/context'
 import type { INotifyApp, INotifyProject } from '../types'
 import { EXPLORER_API_BASE_URL, EXPLORER_ENDPOINTS } from '../constants'
 
+const projectId: string = import.meta.env.VITE_PROJECT_ID
+
 const useNotifyProjects = () => {
+  const [loading, setLoading] = useState(false)
   const [projects, setProjects] = useState<INotifyApp[]>([])
   const { isDevModeEnabled, filterAppDomain } = useContext(SettingsContext)
 
   useEffect(() => {
     const fetchNotifyProjects = async () => {
-      const projectId: string = import.meta.env.VITE_PROJECT_ID
+      setLoading(true)
 
       const explorerUrl = new URL(
         filterAppDomain ? EXPLORER_ENDPOINTS.notifyConfig : EXPLORER_ENDPOINTS.projects,
@@ -26,35 +29,39 @@ const useNotifyProjects = () => {
       const allProjectsRawRes = await fetch(explorerUrl)
       const allNotifyProjectsRes = await allProjectsRawRes.json()
 
+      setLoading(false)
+
       const notifyProjects: Omit<INotifyProject, 'app'>[] = filterAppDomain
         ? [allNotifyProjectsRes.data]
         : Object.values(allNotifyProjectsRes.projects)
-      const notifyApps: INotifyApp[] = notifyProjects.map(
-        ({
-          id,
-          name,
-          description,
-          dapp_url,
-          image_url,
-          metadata,
-          is_verified
-        }: Omit<INotifyProject, 'app'>) => ({
-          id,
-          name,
-          description,
-          url: dapp_url,
-          icons: image_url ? [image_url.md] : [],
-          colors: metadata?.colors,
-          isVerified: is_verified
-        })
-      )
+      const notifyApps: INotifyApp[] = notifyProjects
+        .map(
+          ({
+            id,
+            name,
+            description,
+            dapp_url,
+            image_url,
+            metadata,
+            is_verified
+          }: Omit<INotifyProject, 'app'>) => ({
+            id,
+            name,
+            description,
+            url: dapp_url,
+            icons: image_url ? [image_url.md] : [],
+            colors: metadata?.colors,
+            isVerified: is_verified
+          })
+        )
+        .filter(app => Boolean(app.name))
 
       setProjects(notifyApps)
     }
     fetchNotifyProjects()
   }, [isDevModeEnabled, setProjects, filterAppDomain])
 
-  return projects
+  return { projects, loading }
 }
 
 export default useNotifyProjects
